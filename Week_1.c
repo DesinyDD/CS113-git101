b1897ca881d23c7ad3c6c9d310290d81d1c5e9ea
DesinyDD <panupat.ch@ku.th>
2020-02-13 19:39:44 +0700

    Basic C

---
+++ Week_1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#define MAXIMUM "OMGG"
+float e;
+char r = '\0';
+int main()
+{
+    // printf("Hell World!");
+    int a = 400;
+    char b = 'a';
+    char c = 97;
+    float f = 0.1245;
+    double d = 20.18888;
+    // printf("%c %d %c %d", b, b, c, c);
+    int ans1 = a + f;
+    float ans2 = a + f;
+    
+    printf("%d %f", ans1, ans2);
+    // printf("%c11", r);
+}
